ac9cecff841bdda5329910ab0694d324923ec4ea
rokkrav <mailtoravone@gmail.com>
2024-10-13 16:39:37 +0530

    added new db and schema

--- main.tf
+++ main.tf
@@ -15,3 +15,12 @@ resource "snowflake_database" "SF_DEMO" {
   provider = snowflake.sys_admin
   name = "SF_DEMO"
 }
+
+resource "snowflake_database" "CUSTOM_DB" {
+  name = "CUSTOM_DB"
+}
+
+resource "snowflake_schema" "schema" {
+  name = "CUSTOM_SCHEMA"
+  database = "${snowflake_database.database.name}"
+}
